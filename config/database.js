1a58e6865857ed9bbbc7d29bcb46f40779d21d47
srinivasapraneeth <100212054+srinivasapraneeth@users.noreply.github.com>
2024-09-30 13:48:15 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,1 +1,14 @@
+// config/database.js
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize('user_address_db', 'root', 'password', {
+  host: 'localhost',
+  dialect: 'mysql',
+});
+
+sequelize.authenticate()
+  .then(() => console.log('Database connected...'))
+  .catch(err => console.log('Error: ' + err));
+
+module.exports = sequelize;
 
