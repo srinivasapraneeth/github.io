afd5b22e34cff13a657b9c15ce1318acc4ce5164
srinivasapraneeth <100212054+srinivasapraneeth@users.noreply.github.com>
2024-09-30 13:49:32 +0530

    Update address.js

--- models/models/address.js
+++ models/models/address.js
@@ -1,1 +1,32 @@
+// models/address.js
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+const User = require('./user');
+
+const Address = sequelize.define('Address', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  address: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  userId: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: User,
+      key: 'id',
+    },
+  },
+}, {
+  timestamps: false,
+});
+
+// Establishing the relationship
+User.hasMany(Address, { foreignKey: 'userId' });
+Address.belongsTo(User, { foreignKey: 'userId' });
+
+module.exports = Address;
 
