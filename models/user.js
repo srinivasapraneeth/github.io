361573180bb95fe58fb4841f6eb19cc43d3d85de
srinivasapraneeth <100212054+srinivasapraneeth@users.noreply.github.com>
2024-09-30 13:49:06 +0530

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,1 +1,20 @@
+// models/user.js
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+
+const User = sequelize.define('User', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+}, {
+  timestamps: false,
+});
+
+module.exports = User;
 
